047f0964415fea03cda2f76a068bc458381132e4
Sylvia Simon <sylvia.simon@cremedigital.com>
2019-12-13 11:51:08 +0800

    Dynamic Ads required parameters added

--- gtm-library-fb-datalayers.js
+++ gtm-library-fb-datalayers.js
@@ -1,9 +1,22 @@
-function pushTracking(type, event) {
-    console.log(type);
+//references: https://developers.facebook.com/docs/analytics/send_data/events | https://developers.facebook.com/docs/facebook-pixel/implementation/dynamic-ads | https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking
+
+//required parameters for DYNAMIC ADS ONLY
+function pushTracking(VALUEOFBASKET, TYPE, SKU, QTY) {
+    console.log(TYPE);
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({
-        value: type,
-        event: event
+        currency: 'MYR',
+        value: VALUEOFBASKET, //interger or float
+        content_type: TYPE, //string
+        contents: [{ //array of JSON objects, id and quantity are required fields
+                id: SKU, //array of intergers or strings (e.g. ['ABC123', 'XYZ789'])
+                quantity: QTY
+            },
+            {
+                id: SKU,
+                quantity: QTY
+            }
+        ]
     });
 }
 
