9fe84e819b3212465dc7320c01dabc635b003594
Henrik Soederlund <github@henriksoderlund.com>
2019-12-16 12:36:42 +0800

    add google dynamic remarketing parameters to functions

--- example-function-calls.js
+++ example-function-calls.js
@@ -1,15 +1,41 @@
 //EXAMPLE call to attach to AddToCart action on website
-fbDynamicAdsPush(
-    'AddToCart',                    // populated manually: typed as string matching FB's documentation
-    'MYR',                          // populated dynamically: 3-letter currrency code as a string 'MYR' or 'USD' etc
-    210.40,                         // populated dynamically: interger or float of the total basket value
-    ['11234', '11112', '10982']     // populated dynamically: product IDs (or SKU's) associated with the event stored in an array
+dynamicAdsPush(
+    'AddToCart',                                // Facebook: manual: ViewContent, AddToCart, InitiateCheckout, AddPaymentInfo, AddToWishlist, CompleteRegistration, Purchase, Search
+    'add_to_cart',                              // Google: manual: view_search_results, view_item_list, view_item, add_to_cart, purchase
+    'MYR',                                      // populated dynamically: 3-letter currrency code as a string 'MYR' or 'USD' etc
+    210.40,                                     // populated dynamically: interger or float of the total basket value
+    ['11234', '11112', '10982'],                // populated dynamically: product IDs (or SKU's) associated with the event stored in an array
+    [{                                          
+        'id':'11234', 
+        'google_business_vertical': 'retail'
+    },                                          
+    {
+        'id':'11112',                           // product IDs (or SKU's) and google business vertical stored in an array+objects
+        'google_business_vertical': 'retail'
+    },
+    {
+        'id':'10982', 
+        'google_business_vertical': 'retail'
+    }]
 );
 
 //EXAMPLE call to attach to Purchase action on website
-fbDynamicAdsPush(
-    'Purchase',                    // populated manually: typed as string matching FB's documentation
-    'MYR',                          // populated dynamically: 3-letter currrency code as a string 'MYR' or 'USD' etc
-    210.40,                         // populated dynamically: interger or float of the total basket value
-    ['11234', '11112', '10982']     // populated dynamically: product IDs (or SKU's) associated with the event stored in an array
+dynamicAdsPush(
+    'Purchase',                                 // Facebook: manual: ViewContent, AddToCart, InitiateCheckout, AddPaymentInfo, AddToWishlist, CompleteRegistration, Purchase, Search
+    'purchase',                                 // Google: manual: view_search_results, view_item_list, view_item, add_to_cart, purchase
+    'MYR',                                      // populated dynamically: 3-letter currrency code as a string 'MYR' or 'USD' etc
+    210.40,                                     // populated dynamically: interger or float of the total basket value
+    ['11234', '11112', '10982'],                // populated dynamically: product IDs (or SKU's) associated with the event stored in an array
+    [{                                          
+        'id':'11234', 
+        'google_business_vertical': 'retail'
+    },                                          
+    {
+        'id':'11112',                           // product IDs (or SKU's) and google business vertical stored in an array+objects
+        'google_business_vertical': 'retail'
+    },
+    {
+        'id':'10982', 
+        'google_business_vertical': 'retail'
+    }]
 );
